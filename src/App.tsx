06d1e64ac3ffa519a0e2fb093aa0245a277320d2
[dyad] <isgpimenta.adv@gmail.com>
2026-04-29 21:49:39 -0300

    Reverted all changes back to version 7e7554cbab531b7fb3cc09c5e396d2e71c3d2a34

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import EmbrulhoComAmor from "./pages/EmbrulhoComAmor";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -17,8 +16,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/embrulho-com-amor" element={<EmbrulhoComAmor />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-all "*" ROUTE */}
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
